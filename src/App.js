e4b5a6c7ef910fe8f3214d672ca788cde9f53db2
Gina Case <ginamcase@gmail.com>
2022-11-03 18:57:37 -0600

    fixed some of the card functions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { UserContext } from "./components/context";
+import "./App.css";
+import NavBar from "./components/navbar";
+import Home from "./components/home";
+import AllData from "./components/alldata";
+import Deposit from "./components/deposit";
+import CreateAccount from "./components/createaccount";
+import Withdraw from "./components/withdraw";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <UserContext.Provider
+        value={{
+          users: [
+            {
+              name: "User",
+              email: "user@mit.edu",
+              password: "secret456",
+              balance: "0",
+            },
+          ],
+        }}
+      >
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/withdraw" element={<Withdraw />} />
+          <Route path="/deposit" element={<Deposit />} />
+          <Route path="/alldata" element={<AllData />} />
+          <Route path="/createaccount" element={<CreateAccount />} />
+        </Routes>
+      </UserContext.Provider>
+    </BrowserRouter>
   );
 }
 
